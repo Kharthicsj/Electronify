18a5e5e42b7104c11a407cf3d95cf24c942bea0c
Kharthicsj <kharthicsj@gmail.com>
2025-06-09 19:56:29 +0530

    Modified CORS

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: [
         "Authorization",
@@ -23,7 +23,7 @@ app.use(cors({
 
 app.use("/", router)
 
-mongoose.connect("mongodb://localhost:27017/Ecommerce").then(() => {
+mongoose.connect(process.env.MONGO_DB).then(() => {
     console.log("MongoDB connected successfully...")
 }).catch(() => {
     console.log("Error while connecting MongoDB")
